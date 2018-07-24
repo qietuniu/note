53a52d049e27c1fa845fb147efc8cc78101f259f
陈婷 <873394285@qq.com>
2018-07-24 15:30:20 +0800

    724

--- react/cat-app/src/index.js
+++ react/cat-app/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom'; 
 import './index.css';
 import App from './App';
+import TodoList from './TodoList';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
 	<HashRouter>
-		<App/>
+		<TodoList/>
 	</HashRouter>
 	,document.getElementById('root')
 );
