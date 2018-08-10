de3cf0137fc5eef7356ecdeee66dbcef238484db
陈婷 <873394285@qq.com>
2018-08-10 17:54:38 +0800

    routerdemo1

---
+++ react/ctmanager/src/pages/router_demo/router1/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { HashRouter, Route, Link} from 'react-router-dom';
+
+export default class Home extends Component{
+    
+    render(){
+        return (
+            <HashRouter>
+                <div>asdasd </div>
+            </HashRouter>
+        );
+    }
+}
