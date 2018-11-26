b2bea8d1a056b65e711920158dbf7ca08194b9b0
陈婷 <873394285@qq.com>
2018-11-26 16:32:45 +0800

    提交

--- react/redux-demo/README.md
+++ react/redux-demo/README.md
@@ -8,3 +8,10 @@ redux相关内容，移到单独的文件index.redux.js单独管理
 yarn add react-redux
 忘记subscribe，记住reducer,action,dispatch即可
 提供了Provider和connect 两个接口来链接
+
+使用方法：
+Provider组件在应用最外层，只需传入一次store即可
+Connect负责从外部获取组件需要的参数
+connect可以用装饰器的方式来写
+使用装饰器优化connect 代码
+npm run eject弹出个性化配置
--- react/redux-demo/src/App.js
+++ react/redux-demo/src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux'
+import { addGun, removeGun, addGunAsync } from './index.redux'
 class App extends Component{
     constructor(props) {
         super(props)
     }
     render() {
-        const store = this.props.store
-        const num = store.getState()
-        const addGun = this.props.addGun
-        const removeGun = this.props.removeGun
-        const addGunAsync = this.props.addGunAsync
         return (
             <div>
-                <h1>机关枪{num}</h1>
-                <button onClick={ ()=>store.dispatch(addGun()) }>申请武器</button>
-                <button onClick={ ()=>store.dispatch(removeGun()) }>上交武器</button>
-                <button onClick={ ()=>store.dispatch(addGunAsync()) }>拖两天再申请</button>
+                <h1>机关枪{this.props.num}</h1>
+                <button onClick={this.props.addGun}>申请武器</button>
+                <button onClick={this.props.removeGun}>上交武器</button>
+                <button onClick={this.props.addGunAsync}>拖两天再申请</button>
             </div>
             
         )
     }
 }
+const mapStateToProps = (state) => {
+	return {num: state}
+}
+const actionCreators = { addGun, removeGun, addGunAsync } 
 
+App = connect( mapStateToProps, actionCreators )(App)
 export default App
